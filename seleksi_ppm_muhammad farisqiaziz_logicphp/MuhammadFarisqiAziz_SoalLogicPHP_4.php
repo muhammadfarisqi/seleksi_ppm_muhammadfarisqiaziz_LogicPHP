e0236e50f7dbc96cbc40a8780af45b04a409a641
muhammadfarisqi <muhammadfarisqiya@gmail.com>
2021-05-04 10:12:29 +0700

    Jawaban soal logic php no 4

---
+++ seleksi_ppm_muhammad farisqiaziz_logicphp/MuhammadFarisqiAziz_SoalLogicPHP_4.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>MuhammadFarisqiAziz_SoalLogicPHP_4</title>
+  </head>
+  <body>
+    <?php
+echo "<p>1.Bilangan Ganjil</p>";
+echo "<p>2.Bilangan Genap</p>";
+echo "<p>3.Bilangan Ganjil</p>";
+echo "<p>4.Bilangan Genap</p>";
+echo "<p>5.Bilangan Ganjil</p>";
+echo "<p>6.Bilangan Genap</p>";
+echo "<p>7.Bilangan Ganjil</p>";
+echo "<p>8.Bilangan Genap</p>";
+echo "<p>9.Bilangan Ganjil</p>";
+echo "<p>10.Bilangan Genap</p>";
+
+?> 
+  </body>
+</html>
