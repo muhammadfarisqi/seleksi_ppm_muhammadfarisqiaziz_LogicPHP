90bff051ef6f5726d8931d6bd35154b66a6f46c5
muhammadfarisqi <muhammadfarisqiya@gmail.com>
2021-05-04 10:17:23 +0700

    Jawaban soal logic php no 5

---
+++ seleksi_ppm_muhammad farisqiaziz_logicphp/MuhammadFarisqiAziz_SoalLogicPHP_5.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <title>MuhammadFarisqiAziz_SoalLogicPHP_5</title>
+  </head>
+  <body>
+    <?php
+echo "<p>Makanan</p>";
+echo "<p>1.Menu Makanan ke 1</p>";
+echo "<p>2.Menu Makanan ke 2</p>";
+echo "<p>3.Menu Makanan ke 3</p>";
+echo "<p>Minuman</p>";
+echo "<p>1.Menu Minuman ke 1</p>";
+echo "<p>2.Menu Minuman ke 2</p>";
+echo "<p>3.Menu Minuman ke 3</p>";
+echo "<p>Parsel</p>";
+echo "<p>1.Menu Parsel ke 1</p>";
+echo "<p>2.Menu Parsel ke 2</p>";
+echo "<p>3.Menu Parsel ke 3</p>";
+?> 
+  </body>
+</html>
